b410eb06a105f579a5e1913b53004a26eb98a59a
sakimasaoka <58124066+sakimasaoka@users.noreply.github.com>
2019-12-06 21:06:52 +0900

    インクリメンタルサーチ
    インクリメンタルサーチ

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -37,7 +37,7 @@ $(function(){
   }
 
 
-  $('form').on('submit', function(e){
+  $(('.new_message')).on('submit', function(e){
     e.preventDefault()
     var input = new FormData(this);
     var url = $(this).attr('action')
