65a5afde8e01d11408debd4c2d23e6511ecd8d02
sakimasaoka <58124066+sakimasaoka@users.noreply.github.com>
2019-12-10 15:34:25 +0900

    修正
    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'chat-space2'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:sakimasaoka/chat-space.git'
+set :repo_url,  'git@github.com:sakimasaoka/chat-space2.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
