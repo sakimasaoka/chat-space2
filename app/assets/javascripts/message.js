98e011a3259ccd9ee7587578bf11eeee7df6606f
sakimasaoka <58124066+sakimasaoka@users.noreply.github.com>
2019-12-11 20:42:29 +0900

    手直し
    手直し

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -16,7 +16,6 @@ $(function(){
       </div>
       </div>`
 
-
     } else {
       var html =`<div class="message"  data-message_id="${message.id}">
       <div class="user-name">
@@ -40,9 +39,8 @@ $(function(){
     e.preventDefault();
       var url = $(this).attr('action');
       var input = new FormData(this);
+    if (window.location.href.match(/\/groups\/\d+\/messages/))
       
-      
-      if (window.location.href.match(/\/groups\/\d+\/messages/));
       $.ajax({
       url:  url,
       type: 'POST',
@@ -50,23 +48,26 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-    })
+      })
       .done(function(newMessage){
         var html = buildHTML(newMessage);
         $('.main_chat__main').append(html);
         $('.new_message')[0].reset();
         $('.form__submit1').prop('disabled', false);
         $('.main_chat__main').animate({ scrollTop: $('.main_chat__main')[0].scrollHeight});
-    })
+      })
       .fail(function() {
         alert("メッセージ送信に失敗しました");
+      })
   })
-  })
-  function reloadMessages(){		
-		var last_message_id = $('.message:last').data('message_id');
+    
+    var reloadMessages = function() {
+      
+	
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    var last_message_id = $('.message:last').data('message_id');
     var href = 'api/messages'
-    if (window.location.href.match(/\/groups\/\d+\/messages/));
-		$.ajax({
+      $.ajax({
 		url: href,
 		type: 'GET',
 		data:{id: last_message_id},
@@ -84,6 +85,9 @@ $(function(){
 		.fail(function(){
       alert('自動更新に失敗しました');
       });
-      };
+  
+    };
+  }
       setInterval(reloadMessages, 5000);
-      });
+
+  });
